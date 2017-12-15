d86f6bd8c5874090656267e26952023fd7602ddc
Jose Roberto Garcia Leon <zorro5183@live.com.mx>
2017-12-15 15:16:42 -0600

    Primer commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/', function() use($app) {
+    
+	//
+	
+	$arreglo = ["hola" => "mundo"];
+	
+	return $app->json($arreglo);
+});
+
+$app->run();
